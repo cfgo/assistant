24225a6d1ac7843fd2582237816b1d6138ed9db6
TFcfgo <thomas.fuchs@cfgo.de>
2020-04-24 15:52:59 +0200

    dev directory removed

--- dev/run_dev.R
+++
@@ -1,7 +0,0 @@
-# Detach all loaded packages and clean your environment
-golem::detach_all_attached()
-rm(list=ls(all.names = TRUE))
-
-# Document and reload your package
-golem::document_and_reload()
-#remotes::install_github("TFcfgo/CFgOwidgets")
